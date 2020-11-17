b45cf235f849ba8f65009dc53d3f15610d6961b3
Alberto Bongioanni <61777787+abongioanni@users.noreply.github.com>
2020-11-17 18:43:31 +0100

    Sistemare setConstraints.sql

--- Data/setConstraints.sql
+++ Data/setConstraints.sql
@@ -1,16 +1,15 @@
-ALTER TABLE [Driver] ADD [FK_Drivers_Countries] FOREIGN KEY ([country]) REFERENCES [Country] ([iso2]);
+ALTER TABLE [Driver] ADD CONSTRAINT [FK_Drivers_Countries] FOREIGN KEY ([country]) REFERENCES [Country] ([iso2]);
 
-ALTER TABLE [Team] ADD [FK_Teams_Countries] FOREIGN KEY ([country]) REFERENCES [Country] ([iso2]);
+ALTER TABLE [Team] ADD CONSTRAINT [FK_Teams_Countries] FOREIGN KEY ([country]) REFERENCES [Country] ([iso2]);
 
-ALTER TABLE [Circuit] ADD [FK_Circuits_Countries] FOREIGN KEY ([country]) REFERENCES [Country] ([iso2]);
+ALTER TABLE [Circuit] ADD CONSTRAINT [FK_Circuits_Countries] FOREIGN KEY ([country]) REFERENCES [Country] ([iso2]);
 
-ALTER TABLE [Driver] ADD [FK_Teams_Drivers] FOREIGN KEY ([team_id]) REFERENCES [Team] ([id]);
+ALTER TABLE [Driver] ADD CONSTRAINT [FK_Teams_Drivers] FOREIGN KEY ([team_id]) REFERENCES [Team] ([id]);
 
-ALTER TABLE [Race] ADD [FK_Race_Circuits] FOREIGN KEY ([circuit_id]) REFERENCES [Circuit] ([id]);
+ALTER TABLE [Race] ADD CONSTRAINT [FK_Race_Circuits] FOREIGN KEY ([circuit_id]) REFERENCES [Circuit] ([id]);
 
-ALTER TABLE [Result] ADD [FK_Results_Teams] FOREIGN KEY ([team_id]) REFERENCES [Team] ([id]);
+ALTER TABLE [Result] ADD CONSTRAINT [FK_Results_Teams] FOREIGN KEY ([team_id]) REFERENCES [Team] ([id]);
 
-ALTER TABLE [Result] ADD [FK_Results_Drivers] FOREIGN KEY ([driver_id]) REFERENCES [Driver] ([number]);
+ALTER TABLE [Result] ADD CONSTRAINT [FK_Results_Drivers] FOREIGN KEY ([driver_id]) REFERENCES [Driver] ([number]);
 
-ALTER TABLE [Result] ADD [FK_Results_Races] FOREIGN KEY ([race_id]) REFERENCES [Race] ([id]);
-
+ALTER TABLE [Result] ADD CONSTRAINT [FK_Results_Races] FOREIGN KEY ([race_id]) REFERENCES [Race] ([id]);
